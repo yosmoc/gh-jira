b09b1f05093ff1bec1913d6e6d030b5ea97dd58a
Francisco Gonzalez <francisco.gonzalez3@ingka.ikea.com>
2024-01-12 19:00:51 +0100

    feat: implement json struct for jira api

--- main.go
+++ main.go
@@ -1,29 +1,36 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"strings"
 )
 
+type Component struct {
+	Name string `json:"name"`
+}
+
 type JiraIssue struct {
 	Key    string `json:"key"`
 	Fields struct {
-		Summary string `json:"summary"`
-		Status  struct {
-			Name string `json:"name"`
-		} `json:"status"`
+		Summary    string      `json:"summary"`
+		Components []Component `json:"components"`
 	} `json:"fields"`
 }
 
-func getJiraIssues(jiraAPIToken, jiraDomain string) []JiraIssue {
-	url := fmt.Sprintf("https://%s/rest/api/2/search?jql=status='To Do'", jiraDomain)
+type JiraResponse struct {
+	Issues []JiraIssue `json:"issues"`
+}
+
+func getJiraIssues(jiraAPIToken, jiraDomain, boardID string) []JiraIssue {
+	jql := fmt.Sprintf("project=%s AND status='To Do'", boardID)
+	url := fmt.Sprintf("https://%s/rest/api/2/search?jql=%s", jiraDomain, url.QueryEscape(jql))
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -41,34 +48,49 @@ func getJiraIssues(jiraAPIToken, jiraDomain string) []JiraIssue {
 		log.Fatal(err)
 	}
 
-	var jiraIssues []JiraIssue
-	err = json.Unmarshal(body, &jiraIssues)
+	// Check response status code
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Received non-OK response code: %d\nResponse body: %s", resp.StatusCode, body)
+	}
+
+	var jiraResponse JiraResponse
+	err = json.Unmarshal(body, &jiraResponse)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return jiraIssues
+	return jiraResponse.Issues
 }
 
 func selectJiraIssue(issues []JiraIssue) string {
 	var keys []string
 	for _, issue := range issues {
-		keys = append(keys, fmt.Sprintf("%s - %s", issue.Key, issue.Fields.Summary))
+		for _, component := range issue.Fields.Components {
+			if component.Name == "Software Engineering" {
+				keys = append(keys, fmt.Sprintf("%s - %s", issue.Key, issue.Fields.Summary))
+				break
+			}
+		}
 	}
 
-	cmd := exec.Command("fzf", "--height", "50%", "--prompt", "Select a Jira issue: ")
-	cmd.Stdin = strings.NewReader(strings.Join(keys, "\n"))
-
-	var stdout, stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("Error running fzf: %v\n%s", err, stderr.String())
+	for _, key := range keys {
+		fmt.Println(key)
 	}
 
-	return strings.TrimSpace(stdout.String())
+	// cmd := exec.Command("fzf", "--height", "50%", "--prompt", "Select a Jira issue: ")
+	// cmd.Stdin = strings.NewReader(strings.Join(keys, "\n"))
+
+	// var stdout, stderr bytes.Buffer
+	// cmd.Stdout = &stdout
+	// cmd.Stderr = &stderr
+
+	// err := cmd.Run()
+	// if err != nil {
+	// 	log.Fatalf("Error running fzf: %v\n%s", err, stderr.String())
+	// }
+
+	// return strings.TrimSpace(stdout.String())
+	return ""
 }
 
 func createBranch(jiraID, jiraTitle string) {
@@ -110,7 +132,9 @@ func main() {
 		log.Fatal("Please provide a Jira Domain")
 	}
 
-	jiraIssues := getJiraIssues(jiraAPIToken, jiraDomain)
+	boardID := "UTPR"
+
+	jiraIssues := getJiraIssues(jiraAPIToken, jiraDomain, boardID)
 	if len(jiraIssues) == 0 {
 		log.Fatal("No Jira issues with 'To Do' status found.")
 	}
