a64862a1b3cd866da2926fa39e81bf31826f9c49
Francisco Gonzalez <francisco.gonzalez3@ingka.ikea.com>
2024-01-13 03:21:50 +0100

    feat: implement fzf selection

--- main.go
+++ main.go
@@ -12,13 +12,15 @@ import (
 	"strings"
 )
 
+var EMPTY_COMMIT_MESSAGE = "[skip ci] REMOVE ME. EMPTY COMMIT"
+
 type Component struct {
 	Name string `json:"name"`
 }
 
 type JiraIssue struct {
-	Key    string `json:"key"`
-	Fields struct {
+	IssueID string `json:"key"`
+	Fields  struct {
 		Summary    string      `json:"summary"`
 		Components []Component `json:"components"`
 	} `json:"fields"`
@@ -28,8 +30,8 @@ type JiraResponse struct {
 	Issues []JiraIssue `json:"issues"`
 }
 
-func getJiraIssues(jiraAPIToken, jiraDomain, boardID string) []JiraIssue {
-	jql := fmt.Sprintf("project=%s AND status='To Do'", boardID)
+func getJiraIssues(jiraAPIToken, jiraDomain, bID, bStatus string) []JiraIssue {
+	jql := fmt.Sprintf("project=%s AND status=%s", bID, bStatus)
 	url := fmt.Sprintf("https://%s/rest/api/2/search?jql=%s", jiraDomain, url.QueryEscape(jql))
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -48,11 +50,6 @@ func getJiraIssues(jiraAPIToken, jiraDomain, boardID string) []JiraIssue {
 		log.Fatal(err)
 	}
 
-	// Check response status code
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Received non-OK response code: %d\nResponse body: %s", resp.StatusCode, body)
-	}
-
 	var jiraResponse JiraResponse
 	err = json.Unmarshal(body, &jiraResponse)
 	if err != nil {
@@ -62,53 +59,56 @@ func getJiraIssues(jiraAPIToken, jiraDomain, boardID string) []JiraIssue {
 	return jiraResponse.Issues
 }
 
-func selectJiraIssue(issues []JiraIssue) string {
-	var keys []string
+func selectJiraIssue(issues []JiraIssue, iFilter string) (string, string) {
+	keys := make(map[string]JiraIssue)
+	var fzfInput []string
 	for _, issue := range issues {
 		for _, component := range issue.Fields.Components {
-			if component.Name == "Software Engineering" {
-				keys = append(keys, fmt.Sprintf("%s - %s", issue.Key, issue.Fields.Summary))
+			if component.Name == iFilter {
+				key := fmt.Sprintf("%s - %s", issue.IssueID, issue.Fields.Summary)
+				keys[key] = issue
+				fzfInput = append(fzfInput, key)
 				break
 			}
 		}
 	}
 
-	for _, key := range keys {
-		fmt.Println(key)
+	cmd := exec.Command("fzf", "--height", "50%", "--prompt", "Select a Jira issue: ")
+	cmd.Stdin = strings.NewReader(strings.Join(fzfInput, "\n"))
+	cmd.Stderr = os.Stderr
+
+	output, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("Error running fzf: %v", err)
 	}
 
-	// cmd := exec.Command("fzf", "--height", "50%", "--prompt", "Select a Jira issue: ")
-	// cmd.Stdin = strings.NewReader(strings.Join(keys, "\n"))
+	selectedKey := strings.TrimSpace(string(output))
+	selectedIssue := keys[selectedKey]
 
-	// var stdout, stderr bytes.Buffer
-	// cmd.Stdout = &stdout
-	// cmd.Stderr = &stderr
+	return selectedIssue.IssueID, selectedIssue.Fields.Summary
 
-	// err := cmd.Run()
-	// if err != nil {
-	// 	log.Fatalf("Error running fzf: %v\n%s", err, stderr.String())
-	// }
-
-	// return strings.TrimSpace(stdout.String())
-	return ""
+	// Debug
+	// fmt.Printf("Selected issue: %s\n", string(output))
+	// fmt.Printf("Jira issue id: %s\n", strings.SplitN(string(output), " ", 2)[0])
+	// fmt.Printf("Jira issue title: %s\n", strings.Join(strings.SplitN(string(output), " ", 10)[2:], "-"))
 }
 
 func createBranch(jiraID, jiraTitle string) {
-	branchName := fmt.Sprintf("%s/%s", jiraID, strings.ReplaceAll(jiraTitle, " ", "-"))
-	exec.Command("git", "switch", "-c", branchName).Run()
+	// branchName := fmt.Sprintf("%s/%s", jiraID, strings.ReplaceAll(jiraTitle, " ", "-"))
+	// exec.Command("git", "switch", "-c", branchName).Run()
 
-	cmd := exec.Command("git", "rev-parse", "--verify", "HEAD")
-	output, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(output) == 0 {
-		cmd = exec.Command("git", "commit", "--allow-empty", "-m", "[skip ci] REMOVE ME. EMPTY COMMIT", "--no-verify")
-		err = cmd.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	// cmd := exec.Command("git", "rev-parse", "--verify", "HEAD")
+	// output, err := cmd.Output()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if len(output) == 0 {
+	// 	cmd = exec.Command("git", "commit", "--allow-empty", "-m", EMPTY_COMMIT_MESSAGE, "--no-verify")
+	// 	err = cmd.Run()
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
 }
 
 // func createPR(jiraID, jiraTitle string) {
@@ -132,25 +132,29 @@ func main() {
 		log.Fatal("Please provide a Jira Domain")
 	}
 
-	boardID := "UTPR"
+	var boardID = "UTPR"                     // Utsikt - Presence Current Sprint.
+	var boardStatus = "'To Do'"              // Issue Status, must be in single quotes.
+	var issueFilter = "Software Engineering" // Issue Filter.
 
-	jiraIssues := getJiraIssues(jiraAPIToken, jiraDomain, boardID)
+	jiraIssues := getJiraIssues(jiraAPIToken, jiraDomain, boardID, boardStatus)
 	if len(jiraIssues) == 0 {
 		log.Fatal("No Jira issues with 'To Do' status found.")
 	}
 
-	selectedIssue := selectJiraIssue(jiraIssues)
-	if selectedIssue == "" {
+	issueID, issueSummary := selectJiraIssue(jiraIssues, issueFilter)
+	if issueID == "" || issueSummary == "" {
 		log.Fatal("No Jira issue selected. Manual entry is required.")
 	}
 
-	// Extract Jira ID and Title from the selected issue
-	parts := strings.SplitN(selectedIssue, " - ", 2)
-	if len(parts) != 2 {
-		log.Fatal("Error parsing selected Jira issue.")
-	}
-	jiraID, jiraTitle := parts[0], parts[1]
+	fmt.Printf("Selected issue: %s - %s", issueID, issueSummary)
 
-	createBranch(jiraID, jiraTitle)
+	// // Extract Jira ID and Title from the selected issue
+	// parts := strings.SplitN(selectedIssue, " - ", 2)
+	// if len(parts) != 2 {
+	// 	log.Fatal("Error parsing selected Jira issue.")
+	// }
+	// jiraID, jiraTitle := parts[0], parts[1]
+
+	// createBranch(jiraID, jiraTitle)
 	// createPR(jiraID, jiraTitle)
 }
